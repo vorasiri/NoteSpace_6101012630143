04b661d1eee5e5d601827c562523bc0717f0229c
vorasiri <s6101012630143@email.kmutnb.ac.th>
2020-04-17 00:11:35 +0700

    Add comments to views.py

--- notes/views.py
+++ notes/views.py
@@ -9,13 +9,16 @@ from django.template.loader import get_template
 
 def clear_string_space(input):
     return ' '.join(input.split())
+# Clear the too many spaces
 
 def home_page(request):
     notes = Note.objects.order_by('-upload_time')
     return render(request,'home_page.html',{'notes':notes})
+# Render home page with all notes
 
 def upload_page(request):
     return render(request,'upload_page.html')
+# Render upload page
 
 def upload_api(request):
     filetype_list = ['img', 'png', 'jpg', 'jpeg', 'tiff', 'gif', 'bmp']
@@ -25,7 +28,7 @@ def upload_api(request):
         newnote.owner = clear_string_space(request.POST['ownername'])
         newnote.desc = clear_string_space(request.POST['desc'])
         newnote.save()
-
+# Save note name, owner name and description to Note mode
         i = 0
         for file in request.FILES.getlist('myfile'):
             if(len(file.name.split('.')) > 1 and
@@ -36,11 +39,11 @@ def upload_api(request):
                 newimg.note = newnote
                 newimg.save()
                 i += 1
-        
+# Check file type for all uploaded files and save Image model
         if(i == 0): #all file is invalid
             newnote.delete()
             return HttpResponse('File Type Error')
-        
+# Return 'File Type Error' if none of these files aren't Image File
         return HttpResponseRedirect('/')
     return HttpResponseRedirect('/')
 
@@ -49,12 +52,15 @@ def detail(request,note_index):
     images = Image.objects.filter(note=n)
     img_url = [i.image.url for i in images]
     return render(request,'detail.html',{'images_url':img_url,'note':n})
+# Render detail page with all note images
 
 def about(request):
     return render(request, 'about.html')
+# Render about page
 
 def help(request):
     return render(request, 'help_main.html')
+# Render the main help page showing the list of help topics
 
 def help_detail(request, help_topic):
     try:
@@ -62,6 +68,7 @@ def help_detail(request, help_topic):
         return render(request, 'help/%s.html'%(help_topic) ) 
     except:
         return HttpResponseNotFound('<h1>404 Page not found</h1>')
+# Render help topic if it exist
 
 def search(request):
     query_word = request.GET.get('q','')
@@ -74,24 +81,25 @@ def search(request):
     {
         'search_key':query_word,
         'searched_notes':searched_notes })
+# Searching by name, description, and tag
 
 def tag_query(request, tag_title):
     query_tag = get_object_or_404(Tag , title=tag_title)
     return render(request, 'tag_result.html',{'tag':query_tag})
+# Show all notes that has relate tag
 
 def addcomment_api(request):
     note_id = request.POST['note_id']
     n = Note.objects.get(id=note_id)
-
+# Get the current note id
     author = request.POST['author']
     text = request.POST['text']
     score = float(request.POST['score']) if request.POST['score'] in ['1','2','3','4','5'] else 0
-
     review = Review()
     review.note = n
     review.author = author
     review.text = text
     review.score = score
-
     review.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+# Save the comment, author and score to Review model
