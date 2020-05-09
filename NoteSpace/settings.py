824738bf1465aeb04ef3815e4d6eab25a9848eaf
vorasiri <s6101012630143@email.kmutnb.ac.th>
2020-05-09 19:45:17 +0700

    Move all URLs to NoteSpace/urls.py

--- NoteSpace/settings.py
+++ NoteSpace/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'notes.urls'
+ROOT_URLCONF = 'NoteSpace.urls'
 
 TEMPLATES = [
     {
--- NoteSpace/urls.py
+++ NoteSpace/urls.py
@@ -13,3 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+from notes import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',views.home_page, name='home_page'),
+    url(r'^upload/',views.upload_page, name='upload_page'),
+    url(r'^api/upload/', views.upload_api, name='api_upload'),
+    url(r'^api/addcomment/', views.addcomment_api, name='addcomment_api'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^about/', views.about, name='about'),
+    
+    path('notes/<int:note_index>/', views.detail,name='detail'),
+    path('tag/<str:tag>', views.tag_query, name='tag_query'),
+    path('help/', views.help, name='help'),
+    path('help/<str:help_topic>', views.help_detail, name='help_detail'),
+    path('delete/<int:note_id>/', views.delete, name='delete'),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
